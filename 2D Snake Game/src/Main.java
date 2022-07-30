8f20538ce2019c2ebe0b1c91836a0b3f16b23625
Satenjay <110151791+Satenjay@users.noreply.github.com>
2022-07-30 16:01:48 +0530

    Update Main.java

--- 2D Snake Game/src/Main.java
+++ 2D Snake Game/src/Main.java
@@ -3,7 +3,7 @@ import java.awt.Color;
 import javax.swing.JFrame;
 public class Main {
 public static void main(String[] args) {
-		
+		//Creating Java Frame
 		JFrame f = new JFrame();
 		f.setTitle("Snake Game");
 		f.setBounds(10,10,905,700);
@@ -11,7 +11,7 @@ public static void main(String[] args) {
 		f.setVisible(true);
 		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		f.setBackground(Color.DARK_GRAY);
-		
+		//Adding gameplay object of Gameplay class in the j frame
 		Gameplay gameplay = new Gameplay();
 		f.add(gameplay);
 		
